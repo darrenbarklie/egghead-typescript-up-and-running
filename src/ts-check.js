dfe6b7b45b6f5a5cd4fc12d365609213e1b56fd4
Darren Barklie <darrenbarklie@gmail.com>
2022-01-01 13:32:14 +0000

    @ts-check and JSDoc examples

---
+++ src/ts-check.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+// Add @ts-check to vanilla JavaScript file to enable type checking
+// Use JSDoc syntax to document types
+
+/**
+ *
+ * @param {string} greeting
+ * @return {string}
+ */
+function greet(greeting) {
+  return greeting;
+}
+
+greet("Hey");
